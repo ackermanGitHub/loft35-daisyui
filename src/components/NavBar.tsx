645e907f7b56550da2032dcc658cce20c8e4a3d2
ackerman <julio.sergio2709@gmail.com>
2023-03-24 19:48:01 -0400

    Add: colot-theme selection

--- src/components/NavBar.tsx
+++ src/components/NavBar.tsx
@@ -1,6 +1,40 @@
 import Image from 'next/image';
+import { useState } from 'react';
+
+const themes = [
+  'light',
+  'dark',
+  'cupcake',
+  'bumblebee',
+  'emerald',
+  'corporate',
+  'synthwave',
+  'retro',
+  'cyberpunk',
+  'valentine',
+  'halloween',
+  'garden',
+  'forest',
+  'aqua',
+  'lofi',
+  'pastel',
+  'fantasy',
+  'wireframe',
+  'black',
+  'luxury',
+  'dracula',
+  'cmyk',
+  'autumn',
+  'business',
+  'acid',
+  'lemonade',
+  'night',
+  'coffee',
+  'winter',
+];
 
 const NavBar: React.FC = () => {
+  const [currentTheme, setCurrentTheme] = useState('');
   return (
     <div className="navbar bg-base-100">
       <label htmlFor="my-drawer-3" className="btn btn-square btn-ghost">
@@ -20,6 +54,77 @@ const NavBar: React.FC = () => {
       </label>
       <div className="flex-1">
         <a className="btn btn-ghost normal-case text-xl">Loft35</a>
+      </div>
+      <div className="dropdown dropdown-bottom dropdown-end">
+        <label tabIndex={0} className="btn m-1 btn-ghost">
+          <svg
+            width="20"
+            height="20"
+            xmlns="http://www.w3.org/2000/svg"
+            fill="none"
+            viewBox="0 0 24 24"
+            className="inline-block h-5 w-5 stroke-current md:h-6 md:w-6"
+          >
+            <path
+              strokeLinecap="round"
+              strokeLinejoin="round"
+              strokeWidth="2"
+              d="M7 21a4 4 0 01-4-4V5a2 2 0 012-2h4a2 2 0 012 2v12a4 4 0 01-4 4zm0 0h12a2 2 0 002-2v-4a2 2 0 00-2-2h-2.343M11 7.343l1.657-1.657a2 2 0 012.828 0l2.829 2.829a2 2 0 010 2.828l-8.486 8.485M7 17h.01"
+            ></path>
+          </svg>
+        </label>
+        <div
+          tabIndex={0}
+          className="dropdown-content bg-base-200 text-base-content rounded-t-box rounded-b-box top-px max-h-96 h-[70vh] w-52 overflow-y-auto shadow-2xl mt-4"
+        >
+          <div className="grid grid-cols-1 gap-3 p-3">
+            {themes.map((theme, index) => {
+              return (
+                <button
+                  className="outline-base-content overflow-hidden rounded-lg text-left"
+                  onClick={() => {
+                    document
+                      .querySelector('html')
+                      ?.setAttribute('data-theme', theme);
+                    setCurrentTheme(theme);
+                  }}
+                  key={index}
+                >
+                  <div
+                    data-theme={theme}
+                    className="bg-base-100 text-base-content w-full cursor-pointer font-sans"
+                  >
+                    <div className="grid grid-cols-5 grid-rows-3">
+                      <div className="col-span-5 row-span-3 row-start-1 flex gap-2 py-3 px-4 items-center">
+                        {currentTheme === theme && (
+                          <svg
+                            xmlns="http://www.w3.org/2000/svg"
+                            width="16"
+                            height="16"
+                            viewBox="0 0 24 24"
+                            fill="black"
+                            className="w-3 h-3"
+                          >
+                            <path d="M20.285 2l-11.285 11.567-5.286-5.011-3.714 3.716 9 8.728 15-15.285z"></path>
+                          </svg>
+                        )}
+                        <div className="flex-grow text-sm font-bold">
+                          {theme}
+                        </div>
+                        <div className="flex flex-shrink-0 flex-wrap gap-1 h-full">
+                          <div className="bg-primary w-2 rounded"></div>
+                          <div className="bg-secondary w-2 rounded"></div>
+                          <div className="bg-accent w-2 rounded"></div>
+                          <div className="bg-neutral w-2 rounded"></div>
+                        </div>
+                      </div>
+                    </div>
+                  </div>
+                </button>
+              );
+            })}
+          </div>
+        </div>
       </div>
       <div className="flex-none">
         <div className="dropdown dropdown-end">
