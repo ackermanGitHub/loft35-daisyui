719bdb52db8ef25dd2d63ef87c2e9f61b532d1da
ackerman <julio.sergio2709@gmail.com>
2023-03-25 01:50:34 -0400

    Add: products page and productUpdate tRPC

---
+++ src/pages/products.tsx
@@ -0,0 +1,55 @@
+import { type Product, type Category, type Image } from '@prisma/client';
+import Head from 'next/head';
+import ProductTable from '~/components/ProductTable';
+import Layout from '~/layout/Layout';
+// import { z } from 'zod';
+// import { prisma } from '~/server/db';
+
+// eslint-disable-next-line @typescript-eslint/require-await
+export async function getServerSideProps() {
+  // const products = await prisma.product.findMany();
+  // const categories = await prisma.category.findMany();
+  // const images = await prisma.image.findMany();
+
+  const products: Product[] = [];
+  const categories: Category[] = [];
+  const images: Image[] = [];
+
+  return {
+    props: { products, categories, images }, // will be passed to the page component as props
+  };
+}
+
+const Home = ({
+  products,
+  categories,
+  images,
+}: {
+  products: Product[];
+  categories: Category[];
+  images: Image[];
+}) => {
+  console.log(products);
+  console.log(categories);
+  console.log(images);
+
+  return (
+    <>
+      <Head>
+        <title>Loft 35 - Products</title>
+        <meta name="description" content="Loft-35 Store" />
+        <meta name="viewport" content="width=device-width, initial-scale=1" />
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+      <Layout>
+        <ProductTable
+          products={products}
+          categories={categories}
+          images={images}
+        />
+      </Layout>
+    </>
+  );
+};
+
+export default Home;
---
+++ src/server/api/routers/productUpdate.ts
@@ -0,0 +1,51 @@
+import { z } from 'zod';
+import { createTRPCRouter, publicProcedure } from '../trpc';
+
+export const productUpdateRouter = createTRPCRouter({
+  updateName: publicProcedure
+    .input(z.object({ productID: z.number(), newName: z.string() }))
+    .query(({ ctx, input }) => {
+      return ctx.prisma.product.update({
+        where: {
+          id: input.productID,
+        },
+        data: {
+          name: input.newName,
+        },
+      });
+    }),
+
+  updateDescription: publicProcedure
+    .input(z.object({ productID: z.number(), newName: z.string() }))
+    .mutation(async ({ ctx, input }) => {
+      return await ctx.prisma.product.update({
+        where: {
+          id: input.productID,
+        },
+        data: {
+          name: input.newName,
+        },
+      });
+    }),
+});
+
+/* 
+model Product {
+  id              Int      @id @default(autoincrement())
+  name            String
+  description     String?
+  primaryImageId  Int      @unique
+  imageUrl        String
+  price           Float
+  priority        Int      @unique @default(autoincrement())
+  active          Boolean
+  deleted         Boolean
+  stock           Int
+  categoryName    String
+  categoryId      Int
+  category        Category @relation(fields: [categoryId], references: [id])
+  primaryImage    Image    @relation(fields: [primaryImageId], references: [id])
+  orders          Order[]  @relation("Product-Orders")
+  secondaryImages Image[]  @relation("secondaryImages")
+}
+*/
