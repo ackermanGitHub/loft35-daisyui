9e4d42471c2c9b72153147416fa92ab393870b3d
ackerman <julio.sergio2709@gmail.com>
2023-03-27 00:52:20 -0400

    edit name finished, for now

--- src/components/ProductTable.tsx
+++ src/components/ProductTable.tsx
@@ -19,20 +19,14 @@ const ProductTable: React.FC<{
   console.log(images);
 
   const [isAnyCheckboxSelected, setIsAnyCheckboxSelected] = useState(false);
-  const [editInputProperties, setEditInputProperties] = useState<{
-    left: number;
-    top: number;
-    width: number;
-    height: number;
-    active: boolean;
-    default: string;
-  }>({
+  const [editInputProperties, setEditInputProperties] = useState({
     left: 0,
     top: 0,
     width: 0,
     height: 0,
     active: false,
-    default: '',
+    value: '',
+    productId: -1,
   });
 
   const {
@@ -47,7 +41,11 @@ const ProductTable: React.FC<{
     },
   });
 
-  const updateProduct = api.updateProduct.updateName.useMutation();
+  const updateProduct = api.updateProduct.updateName.useMutation({
+    onSuccess: () => {
+      void refetchProducts();
+    },
+  });
 
   const toggleActive = api.product.setActive.useMutation({
     onSuccess: () => {
@@ -93,6 +91,12 @@ const ProductTable: React.FC<{
       {editInputProperties?.active && (
         <div className="relative">
           <input
+            onChange={(e) => {
+              setEditInputProperties({
+                ...editInputProperties,
+                value: e.target.value,
+              });
+            }}
             style={{
               left: editInputProperties?.left,
               top: editInputProperties?.top,
@@ -100,7 +104,7 @@ const ProductTable: React.FC<{
               width: editInputProperties?.width,
               display: `${editInputProperties?.active ? 'block' : 'none'}`,
             }}
-            defaultValue={editInputProperties.default}
+            defaultValue={editInputProperties.value}
             className="absolute input input-primary z-20"
           />
           <h2
@@ -176,25 +180,6 @@ const ProductTable: React.FC<{
                         >
                           {product.active ? 'active' : 'disabled'}
                         </div>
-
-                        {/* <span
-                          className="label-text"
-                        >
-                          <svg
-                            viewBox="0 0 16 16"
-                            stroke="currentColor"
-                            style={{
-                              width: '16px',
-                              height: '16px',
-                              display: 'block',
-                              fill: 'rgba(55, 53, 47, 0.85)',
-                              flexShrink: 0,
-                              backfaceVisibility: 'hidden',
-                            }}
-                          >
-                            <path d="M14.7881 2.5752L15.3008 2.04199C15.5605 1.76855 15.5742 1.39941 15.3213 1.13965L15.1436 0.955078C14.9111 0.722656 14.5283 0.763672 14.2754 1.00977L13.749 1.5293L14.7881 2.5752ZM6.68066 9.87598L8.0752 9.28809L14.2891 3.06738L13.25 2.03516L7.03613 8.25586L6.4209 9.60254C6.35254 9.75977 6.52344 9.9375 6.68066 9.87598ZM4.09668 14.4355H12.0674C13.373 14.4355 14.1387 13.6768 14.1387 12.2207V4.99512L12.7988 6.33496V12.1045C12.7988 12.7676 12.4434 13.1025 11.9854 13.1025H4.17871C3.54297 13.1025 3.19434 12.7676 3.19434 12.1045V4.49609C3.19434 3.83301 3.54297 3.49805 4.17871 3.49805H10.0234L11.3633 2.1582H4.09668C2.62695 2.1582 1.85449 2.91699 1.85449 4.37988V12.2207C1.85449 13.6836 2.62695 14.4355 4.09668 14.4355Z"></path>
-                          </svg>
-                        </span> */}
                       </div>
                     </div>
                   </div>
@@ -213,8 +198,26 @@ const ProductTable: React.FC<{
                   />
                 </td>
                 <th>
-                  <th className="flex justify-between items-center">
+                  <div className="flex justify-between items-center">
                     <p>{product.name}</p>
+                    {editInputProperties.active && (
+                      <div
+                        onClick={() => {
+                          setEditInputProperties({
+                            ...editInputProperties,
+                            active: false,
+                          });
+                          if (product.name === editInputProperties.value)
+                            return;
+                          updateProduct.mutate({
+                            productID: product.id,
+                            newName: editInputProperties.value,
+                          });
+                          product.name = editInputProperties.value;
+                        }}
+                        className="fixed inset-0 w-[100vw] h-[100vh] opacity-30 stroke-slate-600"
+                      ></div>
+                    )}
                     <span
                       onClick={(event) => {
                         event.preventDefault();
@@ -222,14 +225,16 @@ const ProductTable: React.FC<{
                         const target = event.currentTarget as HTMLSpanElement;
                         const parent =
                           target.parentElement as HTMLTableCellElement;
+                        const grandParent =
+                          parent.parentElement as HTMLTableCellElement;
 
                         const position = {
-                          x: parent?.offsetLeft,
-                          y: parent?.offsetTop,
+                          x: grandParent?.offsetLeft,
+                          y: grandParent?.offsetTop,
                         };
                         const size = {
-                          width: parent?.offsetWidth,
-                          height: parent?.offsetHeight,
+                          width: grandParent?.offsetWidth,
+                          height: grandParent?.offsetHeight,
                         };
 
                         console.log('position&size', {
@@ -243,8 +248,9 @@ const ProductTable: React.FC<{
                           top: position.y,
                           height: size.height,
                           width: size.width,
-                          default: product.name,
+                          value: product.name,
                           active: true,
+                          productId: product.id,
                         });
                       }}
                       className="label-text relative"
@@ -258,7 +264,7 @@ const ProductTable: React.FC<{
                         <path d="M14.7881 2.5752L15.3008 2.04199C15.5605 1.76855 15.5742 1.39941 15.3213 1.13965L15.1436 0.955078C14.9111 0.722656 14.5283 0.763672 14.2754 1.00977L13.749 1.5293L14.7881 2.5752ZM6.68066 9.87598L8.0752 9.28809L14.2891 3.06738L13.25 2.03516L7.03613 8.25586L6.4209 9.60254C6.35254 9.75977 6.52344 9.9375 6.68066 9.87598ZM4.09668 14.4355H12.0674C13.373 14.4355 14.1387 13.6768 14.1387 12.2207V4.99512L12.7988 6.33496V12.1045C12.7988 12.7676 12.4434 13.1025 11.9854 13.1025H4.17871C3.54297 13.1025 3.19434 12.7676 3.19434 12.1045V4.49609C3.19434 3.83301 3.54297 3.49805 4.17871 3.49805H10.0234L11.3633 2.1582H4.09668C2.62695 2.1582 1.85449 2.91699 1.85449 4.37988V12.2207C1.85449 13.6836 2.62695 14.4355 4.09668 14.4355Z"></path>
                       </svg>
                     </span>
-                  </th>
+                  </div>
                 </th>
                 <th>
                   {Math.round(product.primaryImage.sizeMb)}
