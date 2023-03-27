95837c19d6d57703c9c2fe91a0c72a482b534b6c
ackerman <julio.sergio2709@gmail.com>
2023-03-27 16:10:39 -0400

    toggle to only 2 themes

--- src/components/NavBar.tsx
+++ src/components/NavBar.tsx
@@ -1,40 +1,27 @@
 import Image from 'next/image';
-import { useState } from 'react';
-
-const themes = [
-  'light',
-  'dark',
-  'cupcake',
-  'bumblebee',
-  'emerald',
-  'corporate',
-  'synthwave',
-  'retro',
-  'cyberpunk',
-  'valentine',
-  'halloween',
-  'garden',
-  'forest',
-  'aqua',
-  'lofi',
-  'pastel',
-  'fantasy',
-  'wireframe',
-  'black',
-  'luxury',
-  'dracula',
-  'cmyk',
-  'autumn',
-  'business',
-  'acid',
-  'lemonade',
-  'night',
-  'coffee',
-  'winter',
-];
+import { useEffect, useState } from 'react';
 
 const NavBar: React.FC = () => {
   const [currentTheme, setCurrentTheme] = useState('');
+
+  useEffect(() => {
+    const prefersDarkMode =
+      window.matchMedia &&
+      window.matchMedia('(prefers-color-scheme: dark)').matches;
+
+    if (prefersDarkMode) {
+      console.log('User prefers dark mode.');
+
+      document.querySelector('html')?.setAttribute('data-theme', 'dracula');
+      setCurrentTheme('dracula');
+    } else {
+      console.log('User prefers light mode.');
+
+      document.querySelector('html')?.setAttribute('data-theme', 'pastel');
+      setCurrentTheme('pastel');
+    }
+  }, []);
+
   return (
     <div className="navbar bg-base-100">
       <label htmlFor="my-drawer-3" className="btn btn-square btn-ghost">
@@ -59,93 +46,39 @@ const NavBar: React.FC = () => {
         <input type="checkbox" />
 
         <svg
-          className="swap-on fill-current w-8 h-8"
+          className="swap-on fill-current w-6 h-6"
           xmlns="http://www.w3.org/2000/svg"
           viewBox="0 0 24 24"
+          onClick={() => {
+            if (currentTheme === 'dracula') {
+              console.log('dracula');
+              document
+                .querySelector('html')
+                ?.setAttribute('data-theme', 'pastel');
+              setCurrentTheme('pastel');
+            }
+          }}
         >
           <path d="M5.64,17l-.71.71a1,1,0,0,0,0,1.41,1,1,0,0,0,1.41,0l.71-.71A1,1,0,0,0,5.64,17ZM5,12a1,1,0,0,0-1-1H3a1,1,0,0,0,0,2H4A1,1,0,0,0,5,12Zm7-7a1,1,0,0,0,1-1V3a1,1,0,0,0-2,0V4A1,1,0,0,0,12,5ZM5.64,7.05a1,1,0,0,0,.7.29,1,1,0,0,0,.71-.29,1,1,0,0,0,0-1.41l-.71-.71A1,1,0,0,0,4.93,6.34Zm12,.29a1,1,0,0,0,.7-.29l.71-.71a1,1,0,1,0-1.41-1.41L17,5.64a1,1,0,0,0,0,1.41A1,1,0,0,0,17.66,7.34ZM21,11H20a1,1,0,0,0,0,2h1a1,1,0,0,0,0-2Zm-9,8a1,1,0,0,0-1,1v1a1,1,0,0,0,2,0V20A1,1,0,0,0,12,19ZM18.36,17A1,1,0,0,0,17,18.36l.71.71a1,1,0,0,0,1.41,0,1,1,0,0,0,0-1.41ZM12,6.5A5.5,5.5,0,1,0,17.5,12,5.51,5.51,0,0,0,12,6.5Zm0,9A3.5,3.5,0,1,1,15.5,12,3.5,3.5,0,0,1,12,15.5Z" />
         </svg>
 
         <svg
-          className="swap-off fill-current w-8 h-8"
+          className="swap-off fill-current w-6 h-6"
           xmlns="http://www.w3.org/2000/svg"
           viewBox="0 0 24 24"
+          onClick={() => {
+            if (currentTheme === 'pastel') {
+              console.log('pastel');
+              document
+                .querySelector('html')
+                ?.setAttribute('data-theme', 'dracula');
+              setCurrentTheme('dracula');
+            }
+          }}
         >
           <path d="M21.64,13a1,1,0,0,0-1.05-.14,8.05,8.05,0,0,1-3.37.73A8.15,8.15,0,0,1,9.08,5.49a8.59,8.59,0,0,1,.25-2A1,1,0,0,0,8,2.36,10.14,10.14,0,1,0,22,14.05,1,1,0,0,0,21.64,13Zm-9.5,6.69A8.14,8.14,0,0,1,7.08,5.22v.27A10.15,10.15,0,0,0,17.22,15.63a9.79,9.79,0,0,0,2.1-.22A8.11,8.11,0,0,1,12.14,19.73Z" />
         </svg>
       </label>
-      <div className="dropdown dropdown-bottom dropdown-end">
-        <label tabIndex={0} className="btn m-1 btn-ghost">
-          <svg
-            width="20"
-            height="20"
-            xmlns="http://www.w3.org/2000/svg"
-            fill="none"
-            viewBox="0 0 24 24"
-            className="inline-block h-5 w-5 stroke-current md:h-6 md:w-6"
-          >
-            <path
-              strokeLinecap="round"
-              strokeLinejoin="round"
-              strokeWidth="2"
-              d="M7 21a4 4 0 01-4-4V5a2 2 0 012-2h4a2 2 0 012 2v12a4 4 0 01-4 4zm0 0h12a2 2 0 002-2v-4a2 2 0 00-2-2h-2.343M11 7.343l1.657-1.657a2 2 0 012.828 0l2.829 2.829a2 2 0 010 2.828l-8.486 8.485M7 17h.01"
-            ></path>
-          </svg>
-        </label>
-        <div
-          tabIndex={0}
-          className="dropdown-content bg-base-200 text-base-content rounded-t-box rounded-b-box top-px max-h-96 h-[70vh] w-52 overflow-y-auto shadow-2xl mt-4"
-        >
-          <div className="grid grid-cols-1 gap-3 p-3">
-            {themes.map((theme, index) => {
-              return (
-                <button
-                  className="outline-base-content overflow-hidden rounded-lg text-left"
-                  onClick={() => {
-                    document
-                      .querySelector('html')
-                      ?.setAttribute('data-theme', theme);
-                    setCurrentTheme(theme);
-                  }}
-                  key={index}
-                >
-                  <div
-                    data-theme={theme}
-                    className="bg-base-100 text-base-content w-full cursor-pointer font-sans"
-                  >
-                    <div className="grid grid-cols-5 grid-rows-3">
-                      <div className="col-span-5 row-span-3 row-start-1 flex gap-2 py-3 px-4 items-center">
-                        {currentTheme === theme && (
-                          <svg
-                            xmlns="http://www.w3.org/2000/svg"
-                            width="16"
-                            height="16"
-                            viewBox="0 0 24 24"
-                            fill="black"
-                            className="w-3 h-3"
-                          >
-                            <path d="M20.285 2l-11.285 11.567-5.286-5.011-3.714 3.716 9 8.728 15-15.285z"></path>
-                          </svg>
-                        )}
-
-                        <div className="flex-grow text-sm font-bold">
-                          {theme}
-                        </div>
-                        <div className="flex flex-shrink-0 flex-wrap gap-1 h-full">
-                          <div className="bg-primary w-2 rounded"></div>
-                          <div className="bg-secondary w-2 rounded"></div>
-                          <div className="bg-accent w-2 rounded"></div>
-                          <div className="bg-neutral w-2 rounded"></div>
-                        </div>
-                      </div>
-                    </div>
-                  </div>
-                </button>
-              );
-            })}
-          </div>
-        </div>
-      </div>
       <div className="flex-none">
         <div className="dropdown dropdown-end">
           <label tabIndex={0} className="btn btn-ghost btn-circle">
