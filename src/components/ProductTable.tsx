97ad1d934a03036f7a9b7909a24dc83dd6fd1184
ackerman <julio.sergio2709@gmail.com>
2023-03-28 11:41:23 -0400

    delete and upload btn to bottom

--- src/components/ProductTable.tsx
+++ src/components/ProductTable.tsx
@@ -361,60 +361,6 @@ const ProductTable: React.FC = () => {
                 </tr>
               );
             })}
-          <tr>
-            <th></th>
-            <td className="flex flex-row justify-around gap-2">
-              <div className="flex items-center space-x-3">
-                <ProductForm
-                  onUploadSucces={() => {
-                    void refetchProducts();
-                  }}
-                />
-              </div>
-              <ModalConfirm
-                onOkFn={() => {
-                  // Getting checkboxes
-                  const checkboxes = document.querySelectorAll(
-                    '.select-checkbox-group'
-                  ) as NodeListOf<HTMLInputElement>;
-                  // Filter selected products ids
-                  const checkedProductsIds = productsData
-                    ?.filter((product, index) => {
-                      return checkboxes[index]?.checked === true;
-                    })
-                    .map((product) => product.id);
-
-                  if (!checkedProductsIds || checkedProductsIds?.length === 0)
-                    return;
-                  deleteProducts.mutate({
-                    productIDs: checkedProductsIds,
-                  });
-                }}
-                okBtnText="Eliminar"
-                isDisabled={
-                  !isAnyCheckboxSelected || productsData?.length === 0
-                }
-                title="Eliminando!"
-                description="Estás segura que deseas eliminar estos productos?"
-              >
-                <svg
-                  width="30px"
-                  height="30px"
-                  viewBox="0 0 24 24"
-                  fill="none"
-                  xmlns="http://www.w3.org/2000/svg"
-                >
-                  <path
-                    d="M5 6.77273H9.2M19 6.77273H14.8M9.2 6.77273V5.5C9.2 4.94772 9.64772 4.5 10.2 4.5H13.8C14.3523 4.5 14.8 4.94772 14.8 5.5V6.77273M9.2 6.77273H14.8M6.4 8.59091V15.8636C6.4 17.5778 6.4 18.4349 6.94673 18.9675C7.49347 19.5 8.37342 19.5 10.1333 19.5H13.8667C15.6266 19.5 16.5065 19.5 17.0533 18.9675C17.6 18.4349 17.6 17.5778 17.6 15.8636V8.59091M9.2 10.4091V15.8636M12 10.4091V15.8636M14.8 10.4091V15.8636"
-                    stroke="currentColor"
-                    strokeWidth="1.5"
-                    strokeLinecap="round"
-                    strokeLinejoin="round"
-                  />
-                </svg>
-              </ModalConfirm>
-            </td>
-          </tr>
         </tbody>
         {/* foot */}
         <tfoot>
@@ -429,6 +375,58 @@ const ProductTable: React.FC = () => {
           </tr>
         </tfoot>
       </table>
+      <div className="fixed">
+        <div className="flex flex-row justify-around gap-2">
+          <div className="w-12"></div>
+          <div className="flex items-center space-x-3">
+            <ProductForm
+              onUploadSucces={() => {
+                void refetchProducts();
+              }}
+            />
+          </div>
+          <ModalConfirm
+            onOkFn={() => {
+              // Getting checkboxes
+              const checkboxes = document.querySelectorAll(
+                '.select-checkbox-group'
+              ) as NodeListOf<HTMLInputElement>;
+              // Filter selected products ids
+              const checkedProductsIds = productsData
+                ?.filter((product, index) => {
+                  return checkboxes[index]?.checked === true;
+                })
+                .map((product) => product.id);
+
+              if (!checkedProductsIds || checkedProductsIds?.length === 0)
+                return;
+              deleteProducts.mutate({
+                productIDs: checkedProductsIds,
+              });
+            }}
+            okBtnText="Eliminar"
+            isDisabled={!isAnyCheckboxSelected || productsData?.length === 0}
+            title="Eliminando!"
+            description="Estás segura que deseas eliminar estos productos?"
+          >
+            <svg
+              width="30px"
+              height="30px"
+              viewBox="0 0 24 24"
+              fill="none"
+              xmlns="http://www.w3.org/2000/svg"
+            >
+              <path
+                d="M5 6.77273H9.2M19 6.77273H14.8M9.2 6.77273V5.5C9.2 4.94772 9.64772 4.5 10.2 4.5H13.8C14.3523 4.5 14.8 4.94772 14.8 5.5V6.77273M9.2 6.77273H14.8M6.4 8.59091V15.8636C6.4 17.5778 6.4 18.4349 6.94673 18.9675C7.49347 19.5 8.37342 19.5 10.1333 19.5H13.8667C15.6266 19.5 16.5065 19.5 17.0533 18.9675C17.6 18.4349 17.6 17.5778 17.6 15.8636V8.59091M9.2 10.4091V15.8636M12 10.4091V15.8636M14.8 10.4091V15.8636"
+                stroke="currentColor"
+                strokeWidth="1.5"
+                strokeLinecap="round"
+                strokeLinejoin="round"
+              />
+            </svg>
+          </ModalConfirm>
+        </div>
+      </div>
     </div>
   );
 };
