d04d32e8c2c069866d51a2ab8bc1001eff57d878
ackerman <julio.sergio2709@gmail.com>
2023-04-10 21:37:19 -0400

    Add: selected theme state, fix hidratation error

--- src/components/ChangeTheme.tsx
+++ src/components/ChangeTheme.tsx
@@ -1,3 +1,5 @@
+import { useEffect, useState } from 'react';
+
 const themes = [
   'light',
   'dark',
@@ -32,15 +34,54 @@ const themes = [
 
 interface IProps {
   onChangeFn: (theme: string) => void;
+  currentTheme: string;
 }
 
-const ChangeTheme: React.FC<IProps> = ({ onChangeFn }) => {
+const ChangeTheme: React.FC<IProps> = ({ onChangeFn, currentTheme }) => {
+  const [selectedTheme, setSelectedTheme] = useState('');
+
+  useEffect(() => {
+    setSelectedTheme(currentTheme);
+  }, [currentTheme]);
+
   return (
     <div>
       <div className="dropdown">
-        <label tabIndex={0} className="select select-info">
-          Theme
+        <label
+          tabIndex={0}
+          className="outline-base-content overflow-hidden rounded-lg text-left"
+          data-set-theme={selectedTheme}
+        >
+          <div
+            data-theme={selectedTheme}
+            className="bg-base-100 text-base-content w-full cursor-pointer font-sans"
+          >
+            <div className="grid grid-cols-5 grid-rows-3">
+              <div className="col-span-5 row-span-3 row-start-1 flex gap-2 py-3 px-4 items-center">
+                <svg
+                  xmlns="http://www.w3.org/2000/svg"
+                  width="16"
+                  height="16"
+                  viewBox="0 0 24 24"
+                  fill="currentColor"
+                  className="w-3 h-3 invisible"
+                >
+                  <path d="M20.285 2l-11.285 11.567-5.286-5.011-3.714 3.716 9 8.728 15-15.285z"></path>
+                </svg>{' '}
+                <div className="flex-grow text-sm font-bold">
+                  {selectedTheme}
+                </div>{' '}
+                <div className="flex flex-shrink-0 flex-wrap gap-1 h-full">
+                  <div className="bg-primary w-2 rounded"></div>{' '}
+                  <div className="bg-secondary w-2 rounded"></div>{' '}
+                  <div className="bg-accent w-2 rounded"></div>{' '}
+                  <div className="bg-neutral w-2 rounded"></div>
+                </div>
+              </div>
+            </div>
+          </div>
         </label>
+
         <div className="dropdown-content bg-base-200 text-base-content rounded-t-box rounded-b-box top-px max-h-96 h-[70vh] w-52 overflow-y-auto shadow-2xl mt-16">
           <div className="grid grid-cols-1 gap-3 p-3" tabIndex={0}>
             {themes.map((theme, index) => (
@@ -50,6 +91,7 @@ const ChangeTheme: React.FC<IProps> = ({ onChangeFn }) => {
                 data-set-theme={theme}
                 onClick={() => {
                   onChangeFn(theme);
+                  setSelectedTheme(theme);
                 }}
               >
                 <div
--- src/components/Settings.tsx
+++ src/components/Settings.tsx
@@ -2,7 +2,7 @@ import ChangeTheme from './ChangeTheme';
 import { useCookies } from 'react-cookie';
 
 const SettingsOptions = () => {
-  const [_cookies, setCookie] = useCookies([
+  const [cookies, setCookie] = useCookies([
     'color-theme',
     'light-theme',
     'dark-theme',
@@ -13,20 +13,32 @@ const SettingsOptions = () => {
       <input type="checkbox" id="setting-modal" className="modal-toggle" />
       <div className="modal">
         <div className="modal-box overflow-visible h-[50vh]">
-          <ChangeTheme
-            onChangeFn={(theme) => {
-              setCookie('light-theme', theme);
-            }}
-          />
-          <ChangeTheme
-            onChangeFn={(theme) => {
-              setCookie('dark-theme', theme);
-            }}
-          />
-          <div className="modal-action">
-            <label htmlFor="setting-modal" className="btn">
-              Ok!
-            </label>
+          <div className="flex h-full flex-col justify-between">
+            <div className="flex flex-row justify-around">
+              <div>
+                <h2>light theme</h2>
+                <ChangeTheme
+                  currentTheme={cookies['light-theme']}
+                  onChangeFn={(theme) => {
+                    setCookie('light-theme', theme);
+                  }}
+                />
+              </div>
+              <div>
+                <h2>dark theme</h2>
+                <ChangeTheme
+                  currentTheme={cookies['dark-theme']}
+                  onChangeFn={(theme) => {
+                    setCookie('dark-theme', theme);
+                  }}
+                />
+              </div>
+            </div>
+            <div className="modal-action">
+              <label htmlFor="setting-modal" className="btn">
+                Ok!
+              </label>
+            </div>
           </div>
         </div>
       </div>
