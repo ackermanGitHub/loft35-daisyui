acb836be8a86c897e647f0479c0e3f3336535dca
ackerman <julio.sergio2709@gmail.com>
2023-04-11 02:18:48 -0400

    selected theme svg active  (remove invisible)

--- src/components/settings/ChangeTheme.tsx
+++ src/components/settings/ChangeTheme.tsx
@@ -74,16 +74,6 @@ const ChangeTheme: React.FC<IProps> = ({
           >
             <div className="grid grid-cols-5 grid-rows-3">
               <div className="col-span-5 row-span-3 row-start-1 flex gap-2 py-3 px-4 items-center">
-                <svg
-                  xmlns="http://www.w3.org/2000/svg"
-                  width="16"
-                  height="16"
-                  viewBox="0 0 24 24"
-                  fill="currentColor"
-                  className="w-3 h-3 invisible"
-                >
-                  <path d="M20.285 2l-11.285 11.567-5.286-5.011-3.714 3.716 9 8.728 15-15.285z"></path>
-                </svg>{' '}
                 <div className="flex-grow text-sm font-bold">
                   {selectedTheme}
                 </div>{' '}
@@ -122,7 +112,9 @@ const ChangeTheme: React.FC<IProps> = ({
                         height="16"
                         viewBox="0 0 24 24"
                         fill="currentColor"
-                        className="w-3 h-3 invisible"
+                        className={`w-3 h-3 ${
+                          selectedTheme !== theme ? 'invisible' : ''
+                        }`}
                       >
                         <path d="M20.285 2l-11.285 11.567-5.286-5.011-3.714 3.716 9 8.728 15-15.285z"></path>
                       </svg>{' '}
