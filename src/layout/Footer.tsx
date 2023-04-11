908e1add9f39e85423c141d63887a963af9f081e
ackerman <julio.sergio2709@gmail.com>
2023-04-11 02:00:08 -0400

    Add: z-index to footer

--- src/layout/Footer.tsx
+++ src/layout/Footer.tsx
@@ -2,7 +2,7 @@ import SettingsOptions from '~/components/settings/Settings';
 
 const Footer = () => {
   return (
-    <footer className="footer-center btm-nav">
+    <footer className="footer-center btm-nav z-[100]">
       <h1 className=" footer-title">Copyright © 2023 Loft-35</h1>
       <SettingsOptions />
     </footer>
