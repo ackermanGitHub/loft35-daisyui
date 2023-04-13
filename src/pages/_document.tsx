c8310f2a24b10a2e6f26bd33ea1a398521fefcc4
ackerman <julio.sergio2709@gmail.com>
2023-04-13 03:39:34 -0400

    Pls work now 😥

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -24,31 +24,7 @@ export default function MyDocument({ dataTheme }: IDocumentProps) {
   );
 }
 
-// `getInitialProps` belongs to `_document` (instead of `_app`),
-// it's compatible with static-site generation (SSG).
 MyDocument.getInitialProps = async (ctx: DocumentContext) => {
-  // Resolution order
-  //
-  // On the server:
-  // 1. app.getInitialProps
-  // 2. page.getInitialProps
-  // 3. document.getInitialProps
-  // 4. app.render
-  // 5. page.render
-  // 6. document.render
-  //
-  // On the server with error:
-  // 1. document.getInitialProps
-  // 2. app.render
-  // 3. page.render
-  // 4. document.render
-  //
-  // On the client
-  // 1. app.getInitialProps
-  // 2. page.getInitialProps
-  // 3. app.render
-  // 4. page.render
-
   const initialProps = await Document.getInitialProps(ctx);
 
   const setting = await prisma.setting.findUnique({
@@ -56,6 +32,12 @@ MyDocument.getInitialProps = async (ctx: DocumentContext) => {
       id: 1,
     },
   });
+
+  if (!setting)
+    return {
+      ...initialProps,
+      dataTheme: 'pastel',
+    };
 
   let dataTheme = setting?.lightTheme;
 
@@ -66,3 +48,26 @@ MyDocument.getInitialProps = async (ctx: DocumentContext) => {
     dataTheme,
   };
 };
+
+/* 
+import Document, {
+  Html,
+  Head,
+  Main,
+  NextScript,
+} from 'next/document';
+
+export default function Document() {
+
+  return (
+    <Html lang="es">
+      <Head />
+      <body>
+        <Main />
+        <NextScript />
+      </body>
+    </Html>
+  );
+}
+
+*/
