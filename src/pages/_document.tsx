3a35e3dff37d277e18a9170608895d042dc42c59
ackerman <julio.sergio2709@gmail.com>
2023-04-12 23:43:35 -0400

    Add: getInitialProps initial theme in _document.tsx

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -1,8 +1,20 @@
-import { Html, Head, Main, NextScript } from 'next/document';
+import Document, {
+  Html,
+  Head,
+  Main,
+  NextScript,
+  type DocumentProps,
+  type DocumentContext,
+} from 'next/document';
+import { prisma } from '~/server/db';
 
-export default function Document() {
+interface IDocumentProps extends DocumentProps {
+  dataTheme: string;
+}
+
+export default function MyDocument({ dataTheme }: IDocumentProps) {
   return (
-    <Html lang="es">
+    <Html data-theme={dataTheme} lang="es">
       <Head />
       <body>
         <Main />
@@ -11,3 +23,52 @@ export default function Document() {
     </Html>
   );
 }
+
+// `getInitialProps` belongs to `_document` (instead of `_app`),
+// it's compatible with static-site generation (SSG).
+MyDocument.getInitialProps = async (ctx: DocumentContext) => {
+  // Resolution order
+  //
+  // On the server:
+  // 1. app.getInitialProps
+  // 2. page.getInitialProps
+  // 3. document.getInitialProps
+  // 4. app.render
+  // 5. page.render
+  // 6. document.render
+  //
+  // On the server with error:
+  // 1. document.getInitialProps
+  // 2. app.render
+  // 3. page.render
+  // 4. document.render
+  //
+  // On the client
+  // 1. app.getInitialProps
+  // 2. page.getInitialProps
+  // 3. app.render
+  // 4. page.render
+
+  const initialProps = await Document.getInitialProps(ctx);
+
+  const setting = await prisma.setting.findUnique({
+    where: {
+      id: 1,
+    },
+  });
+
+  if (!setting)
+    return {
+      ...initialProps,
+      dataTheme: 'light',
+    };
+
+  let dataTheme = setting?.lightTheme;
+
+  if (setting?.defaultTheme === 'dark') dataTheme = setting.darkTheme;
+
+  return {
+    ...initialProps,
+    dataTheme,
+  };
+};
