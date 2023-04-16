0ebf4763b5a28002b5a6b6931848d5daf9b830bb
ackerman <julio.sergio2709@gmail.com>
2023-04-16 13:28:44 -0400

    Testing home page cards

--- src/pages/test.tsx
+++ src/pages/test.tsx
@@ -1,4 +1,5 @@
 import Head from 'next/head';
+import Image from 'next/image';
 import Layout from '~/layout/Layout';
 
 const Tests = () => {
@@ -11,7 +12,32 @@ const Tests = () => {
         <link rel="icon" href="/favicon.ico" />
       </Head>
       <Layout>
-        <div></div>
+        <div className="flex w-full h-full justify-evenly items-center">
+          <div className="card relative h-1/2 w-1/3 shadow-md overflow-hidden">
+            <h2 className="absolute text-lg font-bold w-2/5 top-1/3 left-2">
+              Lorem ipsum dolor, sit amet consectetur adipisicing elit.
+            </h2>
+            <Image
+              src={'/home-promo_1.png'}
+              alt="home-promo_1"
+              width={324 / 2}
+              height={575 / 2}
+              className="absolute bottom-0 right-0"
+            />
+          </div>
+          <div className="card glass relative h-1/2 w-1/3 shadow-md overflow-hidden">
+            <h2 className="absolute text-lg font-bold text-info w-2/5 top-1/3 left-2">
+              Lorem ipsum dolor, sit amet consectetur adipisicing elit.
+            </h2>
+            <Image
+              src={'/home-promo_1.png'}
+              alt="home-promo_1"
+              width={324 / 2}
+              height={575 / 2}
+              className="absolute bottom-0 right-0"
+            />
+          </div>
+        </div>
       </Layout>
     </>
   );
