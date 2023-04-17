401c8aea1c5f2ede5d845dc15adc338fb76aa853
ackerman <julio.sergio2709@gmail.com>
2023-04-17 00:27:34 -0400

    Add: themes initial cookies

--- src/layout/NavBar.tsx
+++ src/layout/NavBar.tsx
@@ -15,6 +15,32 @@ const NavBar: React.FC = () => {
   const { cart } = useCart();
 
   useEffect(() => {
+    const initialTheme = document
+      .querySelector('html')
+      ?.getAttribute('data-theme');
+    const initialLightTheme = document
+      .querySelector('html')
+      ?.getAttribute('data-light-theme');
+    const initialDarkTheme = document
+      .querySelector('html')
+      ?.getAttribute('data-dark-theme');
+
+    if (!cookies['color-theme']) {
+      setCookie(
+        'color-theme',
+        initialTheme === initialLightTheme ? 'light' : 'dark'
+      );
+    }
+    if (!cookies['light-theme']) {
+      setCookie('light-theme', initialLightTheme);
+    }
+    if (!cookies['dark-theme']) {
+      setCookie('dark-theme', initialDarkTheme);
+    }
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, []);
+
+  useEffect(() => {
     if (cookies['color-theme'] === 'light') {
       document
         .querySelector('html')
@@ -92,17 +118,17 @@ const NavBar: React.FC = () => {
             <div className="indicator">
               <svg
                 xmlns="http://www.w3.org/2000/svg"
-                width="20"
-                height="20"
+                width={24}
+                height={24}
                 fill="none"
                 viewBox="0 0 24 24"
                 stroke="currentColor"
+                strokeWidth={2}
               >
                 <path
                   strokeLinecap="round"
                   strokeLinejoin="round"
-                  strokeWidth="2"
-                  d="M3 3h2l.4 2M7 13h10l4-8H5.4M7 13L5.4 5M7 13l-2.293 2.293c-.63.63-.184 1.707.707 1.707H17m0 0a2 2 0 100 4 2 2 0 000-4zm-8 2a2 2 0 11-4 0 2 2 0 014 0z"
+                  d="M16 11V7a4 4 0 00-8 0v4M5 9h14l1 12H4L5 9z"
                 />
               </svg>
               <span className="badge badge-sm indicator-item">
