a0883385085473c925f5f1f554a3163613a76737
ackerman <julio.sergio2709@gmail.com>
2023-04-20 08:24:15 -0400

    Delete: products-view cookies

--- src/pages/products.tsx
+++ src/pages/products.tsx
@@ -1,49 +1,8 @@
-import { useEffect, useState } from 'react';
-import { type GetServerSidePropsContext } from 'next';
 import Head from 'next/head';
 import Layout from '~/layout/Layout';
-import ProductsTable from '~/components/ProductsTable';
 import ProductsCardScroll from '~/components/ProductsCardScroll';
-import { getCookie, setCookie } from 'cookies-next';
-import { useCookies } from 'react-cookie';
 
-export const getServerSideProps = ({ req, res }: GetServerSidePropsContext) => {
-  const productsView = getCookie('products-view', { req, res });
-  if (!productsView) {
-    return {
-      props: {
-        productsView: 'cards',
-      },
-    };
-  }
-  return {
-    props: {
-      productsView,
-    },
-  };
-};
-
-const Products = ({ productsView }: { productsView: string }) => {
-  const [productView, setProductView] = useState(productsView);
-  const [cookies] = useCookies(['products-view']);
-
-  useEffect(() => {
-    const productViewCookie = getCookie('products-view');
-    if (!productViewCookie) {
-      setCookie('products-view', 'cards');
-    } else {
-      if (typeof productViewCookie === 'string')
-        setProductView(productViewCookie);
-    }
-    // eslint-disable-next-line react-hooks/exhaustive-deps
-  }, []);
-
-  useEffect(() => {
-    const productViewCookie: string = cookies['products-view'];
-    setProductView(productViewCookie);
-    // eslint-disable-next-line react-hooks/exhaustive-deps
-  }, [cookies['products-view']]);
-
+const Products = () => {
   return (
     <>
       <Head>
@@ -53,8 +12,7 @@ const Products = ({ productsView }: { productsView: string }) => {
         <link rel="icon" href="/favicon.ico" />
       </Head>
       <Layout>
-        {productView === 'table' && <ProductsTable />}
-        {productView === 'cards' && <ProductsCardScroll />}
+        <ProductsCardScroll />
       </Layout>
     </>
   );
