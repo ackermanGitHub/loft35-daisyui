e9c7c38454a4aad576c63b9c208ec9b8d0c88f28
ackerman <julio.sergio2709@gmail.com>
2023-04-20 08:22:34 -0400

    Add: 404 error page

---
+++ src/pages/404.tsx
@@ -0,0 +1,19 @@
+import Head from 'next/head';
+import Layout from '~/layout/Layout';
+const Error = () => {
+  return (
+    <>
+      <Head>
+        <title>Loft 35 - Error</title>
+        <meta name="description" content="Loft-35 Store" />
+        <meta name="viewport" content="width=device-width, initial-scale=1" />
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+      <Layout>
+        <div>Error Page</div>
+      </Layout>
+    </>
+  );
+};
+
+export default Error;
