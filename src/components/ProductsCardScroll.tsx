3f8796418cf02ad352d4e9c0c0d58ddfdb7988a1
ackerman <julio.sergio2709@gmail.com>
2023-04-22 12:36:46 -0400

    test to productCardScroll

--- src/components/ProductsCardScroll.tsx
+++ src/components/ProductsCardScroll.tsx
@@ -5,62 +5,94 @@ import { api } from '~/utils/api';
 const ProductsCardScroll: React.FC = () => {
   const { addToCart } = useCart();
 
-  const { data: productsData } = api.product.getAll.useQuery();
+  const { data: productsWithPlaceholder, isFetched } =
+    api.product.getAllWithPlaceholders.useQuery();
 
   return (
     <div className="flex flex-wrap items-center justify-around">
-      {productsData &&
-        productsData.map((product) => (
-          <div
-            key={product.id}
-            className="card card-compact w-[45%] glass mt-12"
-          >
-            <figure className="relative overflow-hidden pb-[100%] w-full">
-              <Image
-                className="object-cover"
-                fill
-                sizes="(max-width: 768px) 60vw,
-                      (max-width: 1200px) 40vw,
-                      33vw"
-                quality={60}
-                src={product.imageUrl}
-                alt={product.name}
-              />
-            </figure>
-            <div className="card-body">
-              <h2 className="card-title">{product.name}</h2>
-              <div className="card-actions justify-end">
-                <button
-                  className="btn btn-primary"
-                  onClick={() => {
-                    addToCart({
-                      productId: product.id,
-                      name: product.name,
-                      price: product.price,
-                      quantity: 1,
-                    });
-                  }}
-                >
-                  <svg
-                    xmlns="http://www.w3.org/2000/svg"
-                    height={24}
-                    width={24}
-                    fill="none"
-                    viewBox="0 0 24 24"
-                    stroke="currentColor"
-                    strokeWidth={2}
-                  >
-                    <path
-                      strokeLinecap="round"
-                      strokeLinejoin="round"
-                      d="M16 11V7a4 4 0 00-8 0v4M5 9h14l1 12H4L5 9z"
-                    />
-                  </svg>
-                </button>
+      {!isFetched &&
+        Array(6)
+          .fill(0)
+          .map((_, index) => (
+            <div key={index} className="card card-compact w-[45%] glass mt-12">
+              <div className="pb-[100%] w-full rounded-md animate-pulse bg-current opacity-25"></div>
+              <div className="card-body">
+                <h2 className="w-1/2 h-4 rounded-lg animate-pulse bg-current opacity-25"></h2>
+                <div className="card-actions justify-end">
+                  <button className="btn btn-primary">
+                    <svg
+                      xmlns="http://www.w3.org/2000/svg"
+                      height={24}
+                      width={24}
+                      fill="none"
+                      viewBox="0 0 24 24"
+                      stroke="currentColor"
+                      strokeWidth={2}
+                    >
+                      <path
+                        strokeLinecap="round"
+                        strokeLinejoin="round"
+                        d="M16 11V7a4 4 0 00-8 0v4M5 9h14l1 12H4L5 9z"
+                      />
+                    </svg>
+                  </button>
+                </div>
               </div>
             </div>
+          ))}
+      {productsWithPlaceholder?.map((product) => (
+        <div
+          key={product.product.id}
+          className="card card-compact w-[45%] glass mt-12"
+        >
+          <figure className="relative overflow-hidden pb-[100%] w-full">
+            <Image
+              src={product.src}
+              blurDataURL={product.blurDataURL}
+              alt={product.product.name}
+              placeholder="blur"
+              className="object-cover"
+              fill
+              sizes="(max-width: 768px) 60vw,
+                      (max-width: 1200px) 40vw,
+                      33vw"
+              quality={60}
+            />
+          </figure>
+          <div className="card-body">
+            <h2 className="card-title">{product.product.name}</h2>
+            <div className="card-actions justify-end">
+              <button
+                className="btn btn-primary"
+                onClick={() => {
+                  addToCart({
+                    productId: product.product.id,
+                    name: product.product.name,
+                    price: product.product.price,
+                    quantity: 1,
+                  });
+                }}
+              >
+                <svg
+                  xmlns="http://www.w3.org/2000/svg"
+                  height={24}
+                  width={24}
+                  fill="none"
+                  viewBox="0 0 24 24"
+                  stroke="currentColor"
+                  strokeWidth={2}
+                >
+                  <path
+                    strokeLinecap="round"
+                    strokeLinejoin="round"
+                    d="M16 11V7a4 4 0 00-8 0v4M5 9h14l1 12H4L5 9z"
+                  />
+                </svg>
+              </button>
+            </div>
           </div>
-        ))}
+        </div>
+      ))}
     </div>
   );
 };
--- src/pages/test.tsx
+++ src/pages/test.tsx
@@ -1,13 +1,7 @@
 import Head from 'next/head';
-import Image from 'next/image';
 import Layout from '~/layout/Layout';
 
-import { api } from '~/utils/api';
-
 const Tests = () => {
-  const { data: productsWithPlaceholder, isFetched } =
-    api.product.getAllWithPlaceholders.useQuery();
-
   return (
     <>
       <Head>
@@ -17,84 +11,7 @@ const Tests = () => {
         <link rel="icon" href="/favicon.ico" />
       </Head>
       <Layout>
-        <div className="flex flex-wrap items-center justify-around">
-          {!isFetched &&
-            Array(6)
-              .fill(0)
-              .map((_, index) => (
-                <div
-                  key={index}
-                  className="card card-compact w-[45%] glass mt-12"
-                >
-                  <div className="pb-[100%] w-full rounded-md animate-pulse bg-current opacity-25"></div>
-                  <div className="card-body">
-                    <h2 className="w-1/2 h-4 rounded-lg animate-pulse bg-current opacity-25"></h2>
-                    <div className="card-actions justify-end">
-                      <button className="btn btn-primary">
-                        <svg
-                          xmlns="http://www.w3.org/2000/svg"
-                          height={24}
-                          width={24}
-                          fill="none"
-                          viewBox="0 0 24 24"
-                          stroke="currentColor"
-                          strokeWidth={2}
-                        >
-                          <path
-                            strokeLinecap="round"
-                            strokeLinejoin="round"
-                            d="M16 11V7a4 4 0 00-8 0v4M5 9h14l1 12H4L5 9z"
-                          />
-                        </svg>
-                      </button>
-                    </div>
-                  </div>
-                </div>
-              ))}
-          {productsWithPlaceholder?.map((product) => (
-            <div
-              key={product.product.id}
-              className="card card-compact w-[45%] glass mt-12"
-            >
-              <figure className="relative overflow-hidden pb-[100%] w-full">
-                <Image
-                  src={product.src}
-                  blurDataURL={product.blurDataURL}
-                  alt={product.product.name}
-                  placeholder="blur"
-                  className="object-cover"
-                  fill
-                  sizes="(max-width: 768px) 60vw,
-                      (max-width: 1200px) 40vw,
-                      33vw"
-                  quality={60}
-                />
-              </figure>
-              <div className="card-body">
-                <h2 className="card-title">{product.product.name}</h2>
-                <div className="card-actions justify-end">
-                  <button className="btn btn-primary">
-                    <svg
-                      xmlns="http://www.w3.org/2000/svg"
-                      height={24}
-                      width={24}
-                      fill="none"
-                      viewBox="0 0 24 24"
-                      stroke="currentColor"
-                      strokeWidth={2}
-                    >
-                      <path
-                        strokeLinecap="round"
-                        strokeLinejoin="round"
-                        d="M16 11V7a4 4 0 00-8 0v4M5 9h14l1 12H4L5 9z"
-                      />
-                    </svg>
-                  </button>
-                </div>
-              </div>
-            </div>
-          ))}
-        </div>
+        <div className="flex items-center justify-around"></div>
       </Layout>
     </>
   );
