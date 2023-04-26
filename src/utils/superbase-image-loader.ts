97e283be1b32fea047fd1ee9b141a199abe9661f
ackerman <julio.sergio2709@gmail.com>
2023-04-25 21:49:51 -0400

    add "images" superbase folder to loader

--- src/utils/superbase-image-loader.ts
+++ src/utils/superbase-image-loader.ts
@@ -1,9 +1,15 @@
-const projectId = 'uhvjljbcyqfpccwrvkqx' // your supabase project id
+const projectId = 'uhvjljbcyqfpccwrvkqx'; // your supabase project id
 
-export default function supabaseLoader({ src, width, quality } : {
-    src: string
-  width: number
-  quality?: number | undefined
+export default function supabaseLoader({
+  src,
+  width,
+  quality,
+}: {
+  src: string;
+  width: number;
+  quality?: number | undefined;
 }) {
-  return `https://${projectId}.supabase.co/storage/v1/object/public/${src}?width=${width}&quality=${quality || 75}`
+  return `https://${projectId}.supabase.co/storage/v1/object/public/images/${src}?width=${width}&quality=${
+    quality || 75
+  }`;
 }
