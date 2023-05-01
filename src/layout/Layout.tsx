6a8c331a8b8d512288a5eb58a75b15a568d1f795
ackerman <julio.sergio2709@gmail.com>
2023-05-01 16:14:21 -0400

    Moving NavBAr on top of drawer

--- src/layout/Layout.tsx
+++ src/layout/Layout.tsx
@@ -1,34 +1,35 @@
 import Link from 'next/link';
 import NavBar from './NavBar';
-// in now on ShoppingCart
-// import SettingsOptions from '~/components/settings/Settings';
+// import SettingsOptions from '~/components/settings/Settings'; // is now in ShoppingCart
 
 const Layout: React.FC<{ children: React.ReactNode }> = ({ children }) => {
   return (
-    <div className="drawer drawer-mobile">
-      <input id="my-drawer-3" type="checkbox" className="drawer-toggle" />
-      <div className="drawer-content relative flex flex-col">
-        <NavBar />
-        <section className='flex flex-col items-center'>
-          {children}
-        </section>
-        {/* <SettingsOptions />  in now on ShoppingCart */}
+    <>
+      <NavBar />
+      <div className="drawer drawer-mobile">
+        <input id="my-drawer-3" type="checkbox" className="drawer-toggle" />
+        <div className="drawer-content relative flex flex-col items-center">
+          <section className='flex max-w-screen-md flex-col items-center'>
+            {children}
+          </section>
+          {/* <SettingsOptions />  is now in ShoppingCart */}
+        </div>
+        <div className="drawer-side">
+          <label htmlFor="my-drawer-3" className="drawer-overlay"></label>
+          <ul className="menu p-4 w-80 bg-base-100">
+            <li>
+              <Link href="/products">Productos</Link>
+            </li>
+            <li>
+              <Link href="/orders">Ordenes</Link>
+            </li>
+            <li>
+              <Link href="/test">Pruebas</Link>
+            </li>
+          </ul>
+        </div>
       </div>
-      <div className="drawer-side">
-        <label htmlFor="my-drawer-3" className="drawer-overlay"></label>
-        <ul className="menu p-4 w-80 bg-base-100">
-          <li>
-            <Link href="/products">Productos</Link>
-          </li>
-          <li>
-            <Link href="/orders">Ordenes</Link>
-          </li>
-          <li>
-            <Link href="/test">Pruebas</Link>
-          </li>
-        </ul>
-      </div>
-    </div>
+    </>
   );
 };
 
