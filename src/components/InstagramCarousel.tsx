7bc002ba20d38f78a9b678f3b4b63cc8c6790d44
ackerman <julio.sergio2709@gmail.com>
2023-05-05 16:57:25 -0400

    Adjusting sizes

--- src/components/InstagramCarousel.tsx
+++ src/components/InstagramCarousel.tsx
@@ -1,4 +1,5 @@
 import Image from "next/image";
+import { useTheme } from "~/hooks/useTheme";
 
 const post = {
     date: "APRIL 2",
@@ -56,10 +57,13 @@ const post = {
 }
 
 export default function InstagramCarousel() {
+    const { colorTheme } = useTheme();
 
     return (
-        <div className="flex text-sm/[16px] font-sans items-center rounded-lg justify-center shadow-lg overflow-hidden max-[735px]:flex-col min-[735px]:w-2/3 max-[360px]:w-full w-[360px]">
-            <div className="flex min-[735px]:hidden justify-between w-full p-3 bg-white">
+        <div style={{
+            backgroundColor: colorTheme === "light" ? "white" : "black"
+        }} className="instagram-card-container flex text-sm/[16px] font-sans items-center rounded-lg justify-center shadow-lg overflow-hidden max-w-2xl max-[735px]:flex-col min-[735px]:w-2/3 max-[360px]:w-full w-[360px]">
+            <div className="flex min-[735px]:hidden justify-between w-full p-3">
                 <div className="flex items-center">
                     <div className="flex justify-center items-center relative w-8 h-8">
                         <Image
@@ -85,7 +89,7 @@ export default function InstagramCarousel() {
                     </div>
                 </div>
                 <div className="flex items-center">
-                    <svg aria-label="More options" className="_ab6-" color="rgb(0, 0, 0)" fill="rgb(0, 0, 0)" height="24" role="img" viewBox="0 0 24 24" width="24"><circle cx="12" cy="12" r="1.5"></circle><circle cx="6" cy="12" r="1.5"></circle><circle cx="18" cy="12" r="1.5"></circle></svg>
+                    <svg aria-label="More options" className="_ab6-" color="currentColor" fill="currentColor" height="24" role="img" viewBox="0 0 24 24" width="24"><circle cx="12" cy="12" r="1.5"></circle><circle cx="6" cy="12" r="1.5"></circle><circle cx="18" cy="12" r="1.5"></circle></svg>
                 </div>
             </div>
 
@@ -107,9 +111,9 @@ export default function InstagramCarousel() {
                     ))
                 }
             </div>
-            <div className="flex flex-col w-full min-[735px]::max-w-[40%] bg-white justify-between min-[735px]:h-full">
+            <div className="flex flex-col w-full justify-between min-[735px]:h-full min-[735px]:max-w-[300px] min-w-[270px]">
                 <div className="flex flex-col h-full max-h-96 max-[735px]:hidden w-full">
-                    <div className="flex justify-between w-full pt-3 pb-2 px-3 bg-white">
+                    <div className="flex justify-between w-full pt-3 pb-2 px-3">
                         <div className="flex items-center">
                             <div className="flex justify-center items-center relative w-8 h-8">
                                 <Image
@@ -145,7 +149,7 @@ export default function InstagramCarousel() {
                         </div>
                     </div>
                     <div className="divider w-full my-0"></div>
-                    <div className="flex flex-col w-full h-full overflow-y-auto overflow-hidden scrollbar-none">
+                    <div className="flex flex-col w-full h-full overflow-y-auto scrollbar-none">
                         <div className="flex items-start h-auto py-2 px-3">
                             <div className="flex justify-center items-center relative w-8 h-8">
                                 <Image
@@ -235,14 +239,14 @@ export default function InstagramCarousel() {
                 </div>
 
                 <div className="flex flex-col" >
-                    <div className="flex justify-between w-full p-3 bg-white">
+                    <div className="flex justify-between w-full p-3">
                         <div className="flex">
-                            <svg aria-label="Like" className="mr-3" color="rgb(38, 38, 38)" fill="rgb(38, 38, 38)" height="24" role="img" viewBox="0 0 24 24" width="24"><title>Like</title><path d="M16.792 3.904A4.989 4.989 0 0 1 21.5 9.122c0 3.072-2.652 4.959-5.197 7.222-2.512 2.243-3.865 3.469-4.303 3.752-.477-.309-2.143-1.823-4.303-3.752C5.141 14.072 2.5 12.167 2.5 9.122a4.989 4.989 0 0 1 4.708-5.218 4.21 4.21 0 0 1 3.675 1.941c.84 1.175.98 1.763 1.12 1.763s.278-.588 1.11-1.766a4.17 4.17 0 0 1 3.679-1.938m0-2a6.04 6.04 0 0 0-4.797 2.127 6.052 6.052 0 0 0-4.787-2.127A6.985 6.985 0 0 0 .5 9.122c0 3.61 2.55 5.827 5.015 7.97.283.246.569.494.853.747l1.027.918a44.998 44.998 0 0 0 3.518 3.018 2 2 0 0 0 2.174 0 45.263 45.263 0 0 0 3.626-3.115l.922-.824c.293-.26.59-.519.885-.774 2.334-2.025 4.98-4.32 4.98-7.94a6.985 6.985 0 0 0-6.708-7.218Z"></path><svg aria-label="Like" className="x1lliihq x1n2onr6" color="rgb(38, 38, 38)" fill="rgb(38, 38, 38)" height="24" role="img" viewBox="0 0 24 24" width="24"><title>Like</title><path d="M16.792 3.904A4.989 4.989 0 0 1 21.5 9.122c0 3.072-2.652 4.959-5.197 7.222-2.512 2.243-3.865 3.469-4.303 3.752-.477-.309-2.143-1.823-4.303-3.752C5.141 14.072 2.5 12.167 2.5 9.122a4.989 4.989 0 0 1 4.708-5.218 4.21 4.21 0 0 1 3.675 1.941c.84 1.175.98 1.763 1.12 1.763s.278-.588 1.11-1.766a4.17 4.17 0 0 1 3.679-1.938m0-2a6.04 6.04 0 0 0-4.797 2.127 6.052 6.052 0 0 0-4.787-2.127A6.985 6.985 0 0 0 .5 9.122c0 3.61 2.55 5.827 5.015 7.97.283.246.569.494.853.747l1.027.918a44.998 44.998 0 0 0 3.518 3.018 2 2 0 0 0 2.174 0 45.263 45.263 0 0 0 3.626-3.115l.922-.824c.293-.26.59-.519.885-.774 2.334-2.025 4.98-4.32 4.98-7.94a6.985 6.985 0 0 0-6.708-7.218Z"></path></svg></svg>
-                            <svg aria-label="Comment" className="mr-3" color="rgb(0, 0, 0)" fill="rgb(0, 0, 0)" height="24" role="img" viewBox="0 0 24 24" width="24"><title>Comment</title><path d="M20.656 17.008a9.993 9.993 0 1 0-3.59 3.615L22 22Z" fill="none" stroke="currentColor" strokeLinejoin="round" strokeWidth="2"></path></svg>
-                            <svg aria-label="Share Post" className="mr-3" color="rgb(0, 0, 0)" fill="rgb(0, 0, 0)" height="24" role="img" viewBox="0 0 24 24" width="24"><title>Share Post</title><line fill="none" stroke="currentColor" strokeLinejoin="round" strokeWidth="2" x1="22" x2="9.218" y1="3" y2="10.083"></line><polygon fill="none" points="11.698 20.334 22 3.001 2 3.001 9.218 10.084 11.698 20.334" stroke="currentColor" strokeLinejoin="round" strokeWidth="2"></polygon></svg>
+                            <svg aria-label="Like" className="mr-3" color="currentColor" fill="currentColor" height="24" role="img" viewBox="0 0 24 24" width="24"><title>Like</title><path d="M16.792 3.904A4.989 4.989 0 0 1 21.5 9.122c0 3.072-2.652 4.959-5.197 7.222-2.512 2.243-3.865 3.469-4.303 3.752-.477-.309-2.143-1.823-4.303-3.752C5.141 14.072 2.5 12.167 2.5 9.122a4.989 4.989 0 0 1 4.708-5.218 4.21 4.21 0 0 1 3.675 1.941c.84 1.175.98 1.763 1.12 1.763s.278-.588 1.11-1.766a4.17 4.17 0 0 1 3.679-1.938m0-2a6.04 6.04 0 0 0-4.797 2.127 6.052 6.052 0 0 0-4.787-2.127A6.985 6.985 0 0 0 .5 9.122c0 3.61 2.55 5.827 5.015 7.97.283.246.569.494.853.747l1.027.918a44.998 44.998 0 0 0 3.518 3.018 2 2 0 0 0 2.174 0 45.263 45.263 0 0 0 3.626-3.115l.922-.824c.293-.26.59-.519.885-.774 2.334-2.025 4.98-4.32 4.98-7.94a6.985 6.985 0 0 0-6.708-7.218Z"></path><svg aria-label="Like" className="x1lliihq x1n2onr6" color="currentColor" fill="currentColor" height="24" role="img" viewBox="0 0 24 24" width="24"><title>Like</title><path d="M16.792 3.904A4.989 4.989 0 0 1 21.5 9.122c0 3.072-2.652 4.959-5.197 7.222-2.512 2.243-3.865 3.469-4.303 3.752-.477-.309-2.143-1.823-4.303-3.752C5.141 14.072 2.5 12.167 2.5 9.122a4.989 4.989 0 0 1 4.708-5.218 4.21 4.21 0 0 1 3.675 1.941c.84 1.175.98 1.763 1.12 1.763s.278-.588 1.11-1.766a4.17 4.17 0 0 1 3.679-1.938m0-2a6.04 6.04 0 0 0-4.797 2.127 6.052 6.052 0 0 0-4.787-2.127A6.985 6.985 0 0 0 .5 9.122c0 3.61 2.55 5.827 5.015 7.97.283.246.569.494.853.747l1.027.918a44.998 44.998 0 0 0 3.518 3.018 2 2 0 0 0 2.174 0 45.263 45.263 0 0 0 3.626-3.115l.922-.824c.293-.26.59-.519.885-.774 2.334-2.025 4.98-4.32 4.98-7.94a6.985 6.985 0 0 0-6.708-7.218Z"></path></svg></svg>
+                            <svg aria-label="Comment" className="mr-3" color="currentColor" fill="currentColor" height="24" role="img" viewBox="0 0 24 24" width="24"><title>Comment</title><path d="M20.656 17.008a9.993 9.993 0 1 0-3.59 3.615L22 22Z" fill="none" stroke="currentColor" strokeLinejoin="round" strokeWidth="2"></path></svg>
+                            <svg aria-label="Share Post" className="mr-3" color="currentColor" fill="currentColor" height="24" role="img" viewBox="0 0 24 24" width="24"><title>Share Post</title><line fill="none" stroke="currentColor" strokeLinejoin="round" strokeWidth="2" x1="22" x2="9.218" y1="3" y2="10.083"></line><polygon fill="none" points="11.698 20.334 22 3.001 2 3.001 9.218 10.084 11.698 20.334" stroke="currentColor" strokeLinejoin="round" strokeWidth="2"></polygon></svg>
                         </div>
                         <div className="flex">
-                            <svg aria-label="Save" className="x1lliihq x1n2onr6" color="rgb(0, 0, 0)" fill="rgb(0, 0, 0)" height="24" role="img" viewBox="0 0 24 24" width="24"><title>Save</title><polygon fill="none" points="20 21 12 13.44 4 21 4 3 20 3 20 21" stroke="currentColor" strokeLinecap="round" strokeLinejoin="round" strokeWidth="2"></polygon></svg>
+                            <svg aria-label="Save" className="x1lliihq x1n2onr6" color="currentColor" fill="currentColor" height="24" role="img" viewBox="0 0 24 24" width="24"><title>Save</title><polygon fill="none" points="20 21 12 13.44 4 21 4 3 20 3 20 21" stroke="currentColor" strokeLinecap="round" strokeLinejoin="round" strokeWidth="2"></polygon></svg>
                         </div>
                     </div>
                     {post.likes && post.likes.length !== 0 && <div className="pl-3 w-full flex">
@@ -271,12 +275,20 @@ export default function InstagramCarousel() {
                             </h2>
                         </div>
                     </div>}
-                    <div className="flex pl-3 pt-2 w-full pb-3 ">
+                    <div className="flex pl-3 max-[735px]:pb-3 pt-2 w-full">
                         <p className="text-[10px] text-[#737373] font-[-apple-system,BlinkMacSystemFont,SegoeUI,Roboto,Helvetica,Arial,sans-serif]">{post.date}</p>
+                    </div>
+                    <div className="flex justify-between max-[735px]:hidden px-3 h-10 items-center w-full">
+                        <svg aria-label="Emoji" color="currentColor" fill="currentColor" height="24" role="img" viewBox="0 0 24 24" width="24"><title>Emoji</title><path d="M15.83 10.997a1.167 1.167 0 1 0 1.167 1.167 1.167 1.167 0 0 0-1.167-1.167Zm-6.5 1.167a1.167 1.167 0 1 0-1.166 1.167 1.167 1.167 0 0 0 1.166-1.167Zm5.163 3.24a3.406 3.406 0 0 1-4.982.007 1 1 0 1 0-1.557 1.256 5.397 5.397 0 0 0 8.09 0 1 1 0 0 0-1.55-1.263ZM12 .503a11.5 11.5 0 1 0 11.5 11.5A11.513 11.513 0 0 0 12 .503Zm0 21a9.5 9.5 0 1 1 9.5-9.5 9.51 9.51 0 0 1-9.5 9.5Z"></path></svg>
+                        <input placeholder="Add a comment..."
+                            className="input p-0 input-ghost h-6"></input>
+                        <h2 className="font-semibold text-sm color-[rgb(179,219,255)]">
+                            POST
+                        </h2>
                     </div>
                 </div>
 
             </div>
-        </div>
+        </div >
     );
 }
